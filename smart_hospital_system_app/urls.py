40537038fbb64ff73f97ed7f30029116cadfd1dc
abdo-ss1 <abdo-ss1@hotmail.com>
2026-04-08 20:44:07 +0300

    final init

--- smart_hospital_system_app/urls.py
+++ smart_hospital_system_app/urls.py
@@ -5,8 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('ai/', views.ai, name='ai'),
     path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
-    path('doctors/', views.doctors, name='doctors'),
 ]
--- smart_hospital_system_app/views.py
+++ smart_hospital_system_app/views.py
@@ -16,13 +16,3 @@ def register(request):
         # Handle registration logic here
         pass
     return render(request, 'register.html')
-
-<<<<<<< HEAD
-def doctors(request):
-    # Fetch doctors from the database and pass them to the
-    # template for rendering
-    return render(request, 'doctors.html')
-=======
-def ai(request):
-    return render(request, 'Ai.html')
->>>>>>> 9e38d55 (Adding ai page)
